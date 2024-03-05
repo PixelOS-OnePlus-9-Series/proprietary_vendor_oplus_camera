fef309e09c00af8c811e16aeab951ebaf2e2af75
pjgowtham <pjgowtham@gmail.com>
2024-03-06 01:28:59 +0530

    Use prebuilt OnePlus Photos 5.0.76 and override Photos
    Move to /product
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- opluscamera.mk
+++ opluscamera.mk
@@ -6,6 +6,10 @@ PRODUCT_BOOT_JARS += \
     oplus-fwk \
     oplus-support-wrapper
 
+# Gallery
+PRODUCT_PACKAGES += \
+    OnePlusGallery
+
 # Init
 PRODUCT_PACKAGES += \
     init.oplus.camera.rc
@@ -14,6 +18,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/oplus_google_lens_config.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/oplus_google_lens_config.xml \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-oplus.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-oplus.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-product-oplus.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-product-oplus.xml \
     $(LOCAL_PATH)/configs/sysconfig/hiddenapi-package-oplus-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-oplus-whitelist.xml
 
 # Properties
