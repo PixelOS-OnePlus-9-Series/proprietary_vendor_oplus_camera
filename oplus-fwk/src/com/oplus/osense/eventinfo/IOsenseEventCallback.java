9f9812fa0894a8346eae76299b9067a1680c521a
chandu078 <chandudyavanapelli03@gmail.com>
2024-04-10 12:06:42 +0530

    camera: Update from Oneplus 12 (CPH2573_14.0.0.604(EX01))
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

---
+++ oplus-fwk/src/com/oplus/osense/eventinfo/IOsenseEventCallback.java
@@ -0,0 +1,24 @@
+package com.oplus.osense.eventinfo;
+
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.IInterface;
+import android.os.Parcel;
+import android.os.RemoteException;
+
+public interface IOsenseEventCallback extends IInterface {
+
+    public static class Default implements IOsenseEventCallback {
+        @Override // android.os.IInterface
+        public IBinder asBinder() {
+            return null;
+        }
+    }
+
+    public static abstract class Stub extends Binder implements IOsenseEventCallback {
+        @Override // android.os.IInterface
+        public IBinder asBinder() {
+            return this;
+        }
+    }
+}
